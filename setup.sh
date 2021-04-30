11d5e3a9e5e38d72594c5a5dd8b6fa77b84444cf
Codins <47649480+C0dins@users.noreply.github.com>
2021-04-30 12:18:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,13 +27,12 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j DROP
 output "Limiting Apache Requests"
 output
 iptables -A INPUT -p tcp --dport 80 -m limit --limit 100/minute --limit-burst 300 -j ACCEPT
-output
 output "Making sure localnetwork is always allowed"
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 output
 output "Done Setting up Iptables now restarting iptables to make sure our new rules work."
-
+sudo /sbin/iptables-save
 output
 
 output "Starting to install fail2ban"
